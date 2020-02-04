68bffb19101ee7f931a4724881289706845a1308
christinaroise <54266462+christinaroise@users.noreply.github.com>
2020-02-04 21:01:37 +0100

    Updates

---
+++ precache-manifest.be2bb2843e736463c5701d7ee9cdb1f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bd757cada0d345abb0d2c393f6cd2a",
+    "url": "/cityguide-oslo/index.html"
+  },
+  {
+    "revision": "bb52dad343c2d3dd1d22",
+    "url": "/cityguide-oslo/static/css/main.0d985a81.chunk.css"
+  },
+  {
+    "revision": "84d7d033413bef4a9001",
+    "url": "/cityguide-oslo/static/js/2.c883093b.chunk.js"
+  },
+  {
+    "revision": "85701e1121fb954a406d391321a3de9e",
+    "url": "/cityguide-oslo/static/js/2.c883093b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb52dad343c2d3dd1d22",
+    "url": "/cityguide-oslo/static/js/main.65bd332e.chunk.js"
+  },
+  {
+    "revision": "6d82070faaa384457ef2",
+    "url": "/cityguide-oslo/static/js/runtime-main.ce54e2f8.js"
+  }
+]);
